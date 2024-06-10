9250e1302668624965ec29eed3857104decb549b
noorzayed <noorzayed204@gmail.com>
2024-06-10 23:36:22 +0300

    noorz final

--- app/src/main/java/com/example/cargo/ReportActivity.java
+++ app/src/main/java/com/example/cargo/ReportActivity.java
@@ -1,151 +1,131 @@
 package com.example.cargo;
 
 import android.os.Bundle;
-import android.widget.Toast;
+import android.util.Log;
+import android.widget.TextView;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.cardview.widget.CardView;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class ReportActivity extends AppCompatActivity {
 
-    private RecyclerView recyclerView;
-    private CarReportAdapter adapter;
-    private List<Car> carList;
-    private RequestQueue requestQueue;
-    private CardView cardView;
+    private TextView mostRentedCarTitleTextView, mostRentedCarNameTextView, mostRentedCarCountTextView;
+    private TextView mostRatedCarTitleTextView, mostRatedCarNameTextView, mostRatedCarCountTextView;
+    private TextView mostPriceCarTitleTextView, mostPriceCarNameTextView, mostPriceCarCountTextView;
+
+    private RequestQueue queue;
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report);
 
-//        cardView = findViewById(R.id.rcardView);
-        recyclerView = findViewById(R.id.recyclerView);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        carList = new ArrayList<>();
-        adapter = new CarReportAdapter(carList);
-        recyclerView.setAdapter(adapter);
+        mostRentedCarTitleTextView = findViewById(R.id.mostRentedCarTitleTextView);
+        mostRentedCarNameTextView = findViewById(R.id.mostRentedCarNameTextView);
+        mostRentedCarCountTextView = findViewById(R.id.mostRentedCarCountTextView);
 
-        requestQueue = Volley.newRequestQueue(this);
-        fetchCarDetails();
+        mostRatedCarTitleTextView = findViewById(R.id.mostRatedCarTitleTextView);
+        mostRatedCarNameTextView = findViewById(R.id.mostRatedCarNameTextView);
+        mostRatedCarCountTextView = findViewById(R.id.mostRatedCarCountTextView);
 
-        // Apply fade-in animation to the CardView
-//        Animation fadeInAnimation = AnimationUtils.loadAnimation(this, R.anim.fade_in);
-//        cardView.startAnimation(fadeInAnimation);
+        mostPriceCarTitleTextView = findViewById(R.id.mostpriceCarTitleTextView);
+        mostPriceCarNameTextView = findViewById(R.id.mostpriceCarNameTextView);
+        mostPriceCarCountTextView = findViewById(R.id.mostpriceCarCountTextView);
+
+        queue = Volley.newRequestQueue(this);
+
+        fetchMostRentedCar();
+        fetchMostRatedCar();
+        fetchMostPriceCar();
     }
 
-    private void fetchCarDetails() {
-        String url = "http://192.168.1.104/android/fetch_cars.php";
-        JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, url, null,
-                new Response.Listener<JSONArray>() {
+    private void fetchMostRentedCar() {
+        String url = "http://192.168.1.104/android/fetch_most_rented.php";
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
                     @Override
-                    public void onResponse(JSONArray response) {
-                        for (int i = 0; i < response.length(); i++) {
-                            try {
-                                JSONObject carObject = response.getJSONObject(i);
-                                int id = carObject.getInt("id");
-                                String brand = carObject.getString("brand");
-                                String location = carObject.getString("location");
-                                int yearModel = carObject.getInt("yearModel");
-                                int seatsNumber = carObject.getInt("seatsNumber");
-                                String transmission = carObject.getString("transmission");
-                                String motorFuel = carObject.getString("motorFuel");
-                                double offeredPrice = carObject.getDouble("offeredPrice");
-                                String image = carObject.getString("image");
-                                byte isRented = (byte) carObject.getInt("isRented");
+                    public void onResponse(JSONObject response) {
+                        try {
+                            String carName = response.getString("car_name");
+                            int rentalCount = response.getInt("rental_count");
 
-                                Car car = new Car(id, brand, location, yearModel, seatsNumber, transmission, motorFuel, offeredPrice, image);
-                                car.setIsRented(isRented);
-                                carList.add(car);
-                            } catch (JSONException e) {
-                                e.printStackTrace();
-                            }
+                            mostRentedCarNameTextView.setText(carName);
+                            mostRentedCarCountTextView.setText(String.valueOf(rentalCount));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
                         }
-
-                        // Generate report
-                        generateReport(carList);
                     }
                 }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                Toast.makeText(ReportActivity.this, "Error fetching data", Toast.LENGTH_SHORT).show();
+                Log.e("Fetch Most Rented Car", "Error: " + error.toString());
             }
         });
-        requestQueue.add(request);
+
+        queue.add(jsonObjectRequest);
     }
 
-    private void generateReport(List<Car> carList) {
-        // Process the carList to generate the report
-        // You can find the most rented car, least rented cars, and cars rented today here
-        // Example:
-        // Find the most rented car
-        Car mostRentedCar = findMostRentedCar(carList);
+    private void fetchMostRatedCar() {
+        String url = "http://192.168.1.104/android/fetch_most_rated.php";
 
-        // Find the least rented cars
-        List<Car> leastRentedCars = findLeastRentedCars(carList);
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            String carName = response.getString("car_name");
+                            double avgRating = response.getDouble("avg_rating");
 
-        // Find cars rented today
-        List<Car> carsRentedToday = findCarsRentedToday(carList);
+                            mostRatedCarNameTextView.setText(carName);
+                            mostRatedCarCountTextView.setText(String.valueOf(avgRating));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("Fetch Most Rated Car", "Error: " + error.toString());
+            }
+        });
 
-        // Display or use the generated report as needed
+        queue.add(jsonObjectRequest);
     }
 
-    private Car findMostRentedCar(List<Car> carList) {
-        // Implement logic to find the most rented car
-        // Example:
-        Car mostRentedCar = null;
-        int maxRentalCount = 0;
-        for (Car car : carList) {
-//            if (car.getRentalCount() > maxRentalCount) {
-//                mostRentedCar = car;
-//                maxRentalCount = car.getRentalCount();
-//            }
-        }
-        return mostRentedCar;
-    }
+    private void fetchMostPriceCar() {
+        String url = "http://192.168.1.104/android/fetch_highest_priced.php";
 
-    private List<Car> findLeastRentedCars(List<Car> carList) {
-        // Implement logic to find the least rented cars
-        // Example:
-        List<Car> leastRentedCars = new ArrayList<>();
-        int minRentalCount = Integer.MAX_VALUE;
-        for (Car car : carList) {
-//            if (car.getRentalCount() < minRentalCount) {
-//                leastRentedCars.clear();
-//                leastRentedCars.add(car);
-//                minRentalCount = car.getRentalCount();
-//            } else if (car.getRentalCount() == minRentalCount) {
-//                leastRentedCars.add(car);
-//            }
-        }
-        return leastRentedCars;
-    }
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            String carName = response.getString("brand");
+                            double offeredPrice = response.getDouble("offered_price");
 
-    private List<Car> findCarsRentedToday(List<Car> carList) {
-        // Implement logic to find cars rented today
-        // Example:
-        List<Car> carsRentedToday = new ArrayList<>();
-        for (Car car : carList) {
-//            if (car.isRentedToday()) {
-//                carsRentedToday.add(car);
-//            }
-        }
-        return carsRentedToday;
-    }}
+                            mostPriceCarNameTextView.setText(carName);
+                            mostPriceCarCountTextView.setText(String.valueOf(offeredPrice));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("Fetch Highest Priced Car", "Error: " + error.toString());
+            }
+        });
+
+        queue.add(jsonObjectRequest);
+    }
+}
